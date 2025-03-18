70e01f23740e8384da1fa5cb14c730d9844f7e0a
Justin Busschau <justin.busschau@gmail.com>
2025-03-18 16:47:33 +0000

    tweaks of favicon and logo

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,33 +1,61 @@
 import type { GatsbyConfig } from "gatsby";
 
+require('dotenv').config();
+
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `The Glade - Welcome to The Grove`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `The Grove`,
+    description: `A mindful CRM platform designed for neurodiverse users and community organizations.`,
+    author: `Justin Busschau`,
+    siteUrl: `https://thegrove.crm`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "dzADvGp_BhB7tCLzWbfOJ7kOGqq96gPOkSfwgUuscMk",
-      "spaceId": "a5jwg42jb1hh"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.tsx"
-    }
-  }, "gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "name": "The Grove",
+        "short_name": "The Grove",
+        "start_url": "/",
+        "background_color": "#000000",
+        "theme_color": "#000000",
+        "display": "minimal-ui",
+        "icon": "src/images/favicon.svg"
+      }
     },
-    __key: "pages"
-  }]
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
+    }
+  ]
 };
 
 export default config;
